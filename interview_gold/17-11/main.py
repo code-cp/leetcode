a7f76fd7246a374b63f74b8ef3e16686b11fe195
code-cp <moshan.nus@gmail.com>
2022-05-27 21:22:13 +0800

    Update main.py

--- interview_gold/17-11/main.py
+++ interview_gold/17-11/main.py
@@ -1,5 +1,20 @@
 from typing import * 
 
+# 有个内含单词的超大文本文件，给定任意两个不同的单词，找出在这个文件中这两个单词的最短距离(相隔单词数)。
+# 如果寻找过程在这个文件中会重复多次，而每次寻找的单词不同，你能对此优化吗?
+
+# 示例：
+
+# 输入：words = ["I","am","a","student","from","a","university","in","a","city"], word1 = "a", word2 = "student"
+# 输出：1
+# 提示：
+
+# words.length <= 100000
+
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode.cn/problems/find-closest-lcci
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
 # TLE 
 class Solution:
     def findClosest(self, words: List[str], word1: str, word2: str) -> int:
@@ -13,8 +28,7 @@ class Solution:
                     p2 += 1
                     if p2 == n: 
                         return min_dist  
-                if words[p2] == word2:
-                    min_dist = min(dist_fn(p1, p2), min_dist)
+                min_dist = min(dist_fn(p1, p2), min_dist)
                 if p2 > p1: 
                     p1 += 1
                     if p1 == n: 
@@ -24,8 +38,7 @@ class Solution:
                     p1 += 1
                     if p1 == n: 
                         return min_dist  
-                if words[p1] == word1:
-                    min_dist = min(dist_fn(p1, p2), min_dist)
+                min_dist = min(dist_fn(p1, p2), min_dist)
                 if p1 > p2: 
                     p2 += 1
                     if p2 == n: 
