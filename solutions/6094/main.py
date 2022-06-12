30a53d7dccd30f34fe44dbcdf3ddd2fa9cb2ffed
code-cp <moshan.nus@gmail.com>
2022-06-12 15:30:55 +0800

    Update main.py

--- solutions/6094/main.py
+++ solutions/6094/main.py
@@ -8,6 +8,8 @@ class Solution:
         for s in ideas: 
             group[s[1:]] |= 1 << (ord(s[0]) - ord('a'))
         ans = 0 
+        # 定义 \textit{cnt}[i][j]cnt[i][j] 表示组中首字母不包含 ii 但包含 jj 的组的个数。枚举每个组，统计 \textit{cnt}cnt，
+        # 同时枚举该组的首字母 ii 和不在该组的首字母 jj，答案即为 \textit{cnt}[i][j]cnt[i][j] 的累加值
         cnt = [[0] * 26 for _ in range(26)] 
         for mask in group.values(): 
             for i in range(26): 
