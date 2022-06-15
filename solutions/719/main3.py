4d4d7576f25ba6cf7cf06ff10531d0fca732fd60
code-cp <moshan.nus@gmail.com>
2022-06-15 21:39:31 +0800

    Update main3.py

--- solutions/719/main3.py
+++ solutions/719/main3.py
@@ -1,6 +1,6 @@
 
 # wrong 
-from queue import PriorityQueue
+import heapq 
 class Solution(object):
     def smallestDistancePair(self, nums, k):
         """
@@ -12,20 +12,22 @@ class Solution(object):
         nums.sort()
         n = len(nums)
         for i in range(1, n):
-            pq.append((i-1, i, nums[i]-nums[i-1]))
+            heapq.heappush(pq, (nums[i]-nums[i-1], i-1, i))
         while len(pq) > 0: 
-            i, j, diff = pq.pop(0) 
+            diff, i, j = heapq.heappop(pq) 
             k -= 1 
             if k == 0: 
                 return diff
             if j < n-1:  
-                pq.append((i, j+1, nums[j+1]-nums[i]))
+                heapq.heappush(pq, (nums[j+1]-nums[i], i, j+1))
             if i > 0:
-                pq.append((i-1, j, nums[j]-nums[i-1]))
+                heapq.heappush(pq, (nums[j]-nums[i-1], i-1, j))
         
 if __name__ == "__main__": 
     s = Solution()
 
-    # assert s.smallestDistancePair([1,6,1], 3) == 5
+    assert s.smallestDistancePair([1,3,1], 1) == 0
+
+    assert s.smallestDistancePair([1,6,1], 3) == 5
 
     assert s.smallestDistancePair([62,100,4], 2) == 58
