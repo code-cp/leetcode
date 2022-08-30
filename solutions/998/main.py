a8e8df6209f14f94e31786e2f01a382c60042cda
code-cp <moshan.nus@gmail.com>
2022-08-30 08:51:00 +0800

    add

---
+++ solutions/998/main.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def insertIntoMaxTree(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root: 
+            return None 
+        tr = TreeNode(val)
+        if root.val < val: 
+            tr.left = root 
+            root = tr 
+            return root 
+        node = root 
+        while node: 
+            if node.right is None:
+                node.right = tr 
+                return root 
+            elif node.right.val < val: 
+                temp = node.right 
+                node.right = tr 
+                tr.left = temp 
+                return root 
+            node = node.right
+        return root   
+                
+            
+            
