1e83ae3650a0ce099163d2f6115517aa14c60597
code-cp <moshan.nus@gmail.com>
2022-10-18 12:09:03 +0800

    Update main.py

--- solutions/902/main.py
+++ solutions/902/main.py
@@ -1,29 +1,56 @@
 from typing import * 
 
+# class Solution:
+#     def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
+#         n_str = str(n)
+#         k = len(n_str)
+
+#         res = 0 
+#         for i in range(1, k):  
+#             res += len(digits) ** i 
+
+#         def dfs(cur, pos, res): 
+#             # base case 
+#             if pos == k: 
+#                 res += 1 
+#                 return res
+
+#             for d in digits: 
+#                 if d < n_str[pos]: 
+#                     res += len(digits) ** (k-1-pos)
+#                 elif d == n_str[pos]:
+#                     res = dfs(cur*10 + int(d), pos+1, res)
+
+#             return res 
+
+#         res = dfs(0, 0, res)
+#         return res 
+
 class Solution:
     def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
         n_str = str(n)
         k = len(n_str)
 
+        # NOTE, need to use global 
+        global res  
         res = 0 
         for i in range(1, k):  
             res += len(digits) ** i 
 
-        def dfs(cur, pos, res): 
+        def dfs(cur, pos):
+            global res  
             # base case 
             if pos == k: 
                 res += 1 
-                return res
+                return 
 
             for d in digits: 
                 if d < n_str[pos]: 
                     res += len(digits) ** (k-1-pos)
                 elif d == n_str[pos]:
-                    res = dfs(cur*10 + int(d), pos+1, res)
+                    dfs(cur*10 + int(d), pos+1)
 
-            return res 
-
-        res = dfs(0, 0, res)
+        dfs(0, 0)
         return res 
             
 if __name__ == "__main__": 
