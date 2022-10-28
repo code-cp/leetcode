8a8abba3d88f1209bad6eddfe7aeb988d22c9972
code-cp <moshan.nus@gmail.com>
2022-10-28 10:19:06 +0800

    add 907

---
+++ solutions/907/main_wrong.py
@@ -0,0 +1,42 @@
+from typing import * 
+
+from collections import deque
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        n = len(arr)
+        M = 1e9 + 7 
+
+        left_min = [0] * n 
+        stack = deque() 
+        stack.append((arr[0], 0))
+        for i in range(1, n): 
+            while len(stack) > 0 and arr[i] < stack[-1][0]:
+                _, j = stack.pop()
+                left_min[i] += j+1  
+            stack.append((arr[i], left_min[i]))
+
+        right_min = [0] * n 
+        stack = deque()
+        stack.append((arr[-1], 0))
+        for i in range(n-2, -1, -1):
+            while len(stack) > 0 and arr[i] < stack[-1][0]:
+                _, j = stack.pop()
+                right_min[i] += j+1  
+            stack.append((arr[i], right_min[i]))
+
+        res = 0 
+        for i in range(n): 
+            res += arr[i] * (((left_min[i]+1) * (right_min[i]+1)) % M) % M 
+            res %= M 
+
+        return int(res) 
+
+if __name__ == "__main__": 
+    s = Solution() 
+
+    # TODO, cannot handle duplicated numbers 
+    arr = [71,55,82,55]
+    assert s.sumSubarrayMins(arr) == 593 
+
+    arr = [3,1,2,4]
+    assert s.sumSubarrayMins(arr) == 17 
---
+++ solutions/907/readme.md
@@ -0,0 +1,10 @@
+- feels similar to `940. Distinct Subsequences II`
+```
+Given a string s, return the number of distinct non-empty subsequences of s. Since the answer may be very large, return it modulo 109 + 7.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not.
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/distinct-subsequences-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+```
